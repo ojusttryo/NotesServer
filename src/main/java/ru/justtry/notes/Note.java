8a60657d1a409cb051ffada979445b542a3f903d
JustTry <ojusttryo@mail.ru>
2019-09-25 06:36:15 +0300

    Change attributes in note to remove extra data

--- src/main/java/ru/justtry/notes/Note.java
+++ src/main/java/ru/justtry/notes/Note.java
@@ -2,19 +2,27 @@ package ru.justtry.notes;
 
 import ru.justtry.shared.Identifiable;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class Note extends Identifiable
 {
     private String folderId;
-    private List<NoteAttribute> attributes;
+    // Such weird data type is because I need to parse array from JSON to key-value type. Something like that:
+    // "attributes": [
+    //		{ "5d8a5a87602f051474ea6a4e": "Avengers 5" },
+    //		{ "5d8a5b17602f051474ea6a50": "Finished" },
+    //		{ "5d8a6035afa6f80313f050c3": 2025 }
+    //	]
+    private List<Map.Entry<String, Object>> attributes = new ArrayList<>();
 
-    public List<NoteAttribute> getAttributes()
+    public List<Map.Entry<String, Object>> getAttributes()
     {
         return attributes;
     }
 
-    public void setAttributes(List<NoteAttribute> attributes)
+    public void setAttributes(List<Map.Entry<String, Object>> attributes)
     {
         this.attributes = attributes;
     }
--- src/main/java/ru/justtry/notes/NoteMapper.java
+++ src/main/java/ru/justtry/notes/NoteMapper.java
@@ -1,6 +1,8 @@
 package ru.justtry.notes;
 
 import com.google.common.base.Strings;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBObject;
 import org.bson.Document;
 import org.bson.types.ObjectId;
 import org.springframework.stereotype.Component;
@@ -28,7 +30,19 @@ public class NoteMapper extends Mapper
         Note note = new Note();
         note.setId(document.get(MONGO_ID).toString());
         note.setFolderId(getStringOrNull(document, FOLDER_ID));
-        note.setAttributes((List<NoteAttribute>)document.get(NoteConstants.ATTRIBUTES));
+        List<Document> attributes = (List<Document>)document.get(NoteConstants.ATTRIBUTES);
+        for (Document attribute : attributes)
+        {
+            for (Map.Entry<String, Object> entry : attribute.entrySet())
+            {
+                note.getAttributes().add(entry);
+                break;  // expected only one
+            }
+//            Object property = attribute.get(0);
+//            Object entry = (Map.Entry<String, Object>)property;
+//            note.getAttributes().add((Map.Entry<String, Object>)entry);
+        }
+        //note.setAttributes((List<Map.Entry<String, Object>>)document.get(NoteConstants.ATTRIBUTES));
 
         return note;
     }
@@ -50,6 +64,21 @@ public class NoteMapper extends Mapper
     @Override
     public Document getDocument(Object object)
     {
+        /**
+         * For now the document saved like this:
+         * {
+         *     "id": "5d8ad8ad43e02d58f3a59945",
+         *     "folderId": null,
+         *     "attributes": [
+         *       { "5d8a5a87602f051474ea6a4e": "Avengers 5" },
+         *       { "5d8a5b17602f051474ea6a50": "Finished" },
+         *       { "5d8a6035afa6f80313f050c3": 2025 }
+         *     ]
+         * }
+         * In the future attributes could be redone to a single object with properties or it could be just
+         * properties of the note object.
+         */
+
         Note note = (Note)object;
 
         Document document = new Document();
@@ -57,13 +86,22 @@ public class NoteMapper extends Mapper
         if (!Strings.isNullOrEmpty(note.getId()))
             document.append(MONGO_ID, new ObjectId(note.getId()));
 
-        List<Document> attributes = new ArrayList<>();
-        for (NoteAttribute attribute : note.getAttributes())
+//        List<Document> attributes = new ArrayList<>();
+        List<DBObject> attributes = new ArrayList<>();
+        for (Map.Entry<String, Object> attribute : note.getAttributes())
         {
-            Document attrDocument = new Document()
-                    .append(NAME, attribute.getName())
-                    .append(VALUE, attribute.getValue());
-            attributes.add(attrDocument);
+            BasicDBObject attr = new BasicDBObject();
+            attr.put(attribute.getKey(), attribute.getValue());
+            attributes.add(attr);
+
+
+//            attributes.add(new Document().append(attribute.getKey(), attribute.getValue()));
+
+//            attributes.append(attribute.getKey(), attribute.getValue());
+//            Document attrDocument = new Document()
+//                    .append(NAME, attribute.getName())
+//                    .append(VALUE, attribute.getValue());
+//            attributes.add(attrDocument);
         }
         document.append(NoteConstants.ATTRIBUTES, attributes);
 
